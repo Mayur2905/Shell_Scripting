c4d3ca2d5898cf024c65fdac8d1c3ce7eb5eb3a9
mayur2905 <mayurgaikwad0101@gmail.com>
2025-08-25 13:04:42 +0000

    comment in shell scripting

---
+++ 02_comment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# single line comments
+
+echo "cheking the comments"
+
+# This is single line comment
+
+
+echo "Checking Multiple Lines Comment"
+
+<<comment
+This is a multiple lines comment
+This is the second line of the comment
+This is the third line of the comment
+comment
---
+++ 03_variablel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Examples of variables
+
+a=10
+name="Mayur"
+age=24
+
+echo "My name is $name and I am $age years old, Love you $a K"
